9a8c7e219f6a4d10b1f37b9113cf9b18b17f3a54
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2024-03-07 10:24:36 -0500

    passed

---
+++ index.js
@@ -0,0 +1,36 @@
+const displayValue = document.getElementById("result")
+const button = document.getElementById("check-btn")
+const inputValue = document.getElementById("text-input")
+
+button.addEventListener("click", checkIfPalindrome)
+
+function checkIfPalindrome () {
+    const regex = /[a-zA-Z0-9]/g
+    const filteredInput = inputValue.value.toLowerCase().match(regex)
+    // let isPalindrome = true;
+    
+    const strLength = filteredInput?.length
+
+
+    function returnResult(){
+
+        for(let i = 0; i < Math.ceil(strLength/2); i++){
+            console.log("check: ", filteredInput[i], filteredInput[i] === filteredInput[strLength - i - 1])
+
+            if(!(filteredInput[i] === filteredInput[strLength - i - 1])){
+                console.log("FAIL")
+                displayValue.innerText = `${inputValue.value + " is not a palindrome"} ` 
+                return 
+            }
+        }
+        displayValue.innerText = `${inputValue.value + " is a palindrome"} ` 
+    }
+
+    if(inputValue.value == "") {
+        alert("Please input a value");
+        return
+    } else {
+        returnResult();
+    }
+
+}
